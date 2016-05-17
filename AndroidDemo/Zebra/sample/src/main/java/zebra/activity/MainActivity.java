d4011698199d6ec85f73208fb03e613c2aaadd58
Inhyuck Im <inhyuck222@gmail.com>
2016-05-18 00:26:14 +0900

    Added Toolbar

--- AndroidDemo/Zebra/sample/src/main/java/zebra/activity/MainActivity.java
+++ AndroidDemo/Zebra/sample/src/main/java/zebra/activity/MainActivity.java
@@ -3,34 +3,80 @@ package zebra.activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import at.markushi.ui.CircleButton;
 import example.zxing.R;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button barcodeButton, reviewButton, loginButton;
+    CircleButton barcodeButton, reviewButton, loginButton;
+    //for toolbar
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
+    ActionBarDrawerToggle mDrawerToggle;
+    String[] mDrawerListItems;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        barcodeButton = (Button) findViewById(R.id.barcodeButton);
-        reviewButton = (Button) findViewById(R.id.reviewButton);
-        loginButton = (Button) findViewById(R.id.loginButton);
+        //Button 설정
+        barcodeButton = (CircleButton) findViewById(R.id.barcodeButton);
+        reviewButton = (CircleButton) findViewById(R.id.reviewButton);
+        loginButton = (CircleButton) findViewById(R.id.searchButton);
 
         barcodeButton.setOnClickListener(this);
         reviewButton.setOnClickListener(this);
         loginButton.setOnClickListener(this);
+
+        //Toolbar 설정
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mDrawerList = (ListView)findViewById(android.R.id.list);
+
+        mDrawerListItems = getResources().getStringArray(R.array.drawer_list);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerListItems));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int editedPosition = position+1;
+                Toast.makeText(MainActivity.this, "You selected item " + editedPosition, Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+                invalidateOptionsMenu();
+                syncState();
+            }
+            public void onDrawerOpened(View v){
+                super.onDrawerOpened(v);
+                invalidateOptionsMenu();
+                syncState();
+            }
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
     }
 
     @Override
@@ -70,6 +116,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(loginIntent);
                 break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home: {
+                if (mDrawerLayout.isDrawerOpen(mDrawerList)){
+                    mDrawerLayout.closeDrawer(mDrawerList);
+                } else {
+                    mDrawerLayout.openDrawer(mDrawerList);
+                }
+                return true;
+            }
+            default: return super.onOptionsItemSelected(item);
         }
     }
 }
