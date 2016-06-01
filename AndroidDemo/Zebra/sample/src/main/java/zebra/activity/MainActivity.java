d4fd2f1891db52de3c342b4a188aade1700030cb
Limky <hot8118@naver.com>
2016-06-01 19:49:27 +0900

    Added GCM

--- AndroidDemo/Zebra/sample/src/main/java/zebra/activity/MainActivity.java
+++ AndroidDemo/Zebra/sample/src/main/java/zebra/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package zebra.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +22,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -26,6 +31,8 @@ import at.markushi.ui.CircleButton;
 import example.zxing.R;
 import zebra.adapters.NaviAdapter;
 import zebra.beans.NaviItem;
+import zebra.gcm.QuickstartPreferences;
+import zebra.gcm.RegistrationIntentService;
 import zebra.json.MyReview;
 import zebra.json.Review;
 import zebra.manager.MemberManager;
@@ -36,6 +43,12 @@ import zebra.views.NaviHeaderView;
 
 
 public class MainActivity extends AppCompatActivity {
+    //GCM
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    String token;
+
     LinearLayout loginButton, barcodeButton, categoryButton, searchButton;
 
     String barcode;
@@ -52,6 +65,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getInstanceIdToken();
+
+        registBroadcastReceiver();
+
 
         loginButton = (LinearLayout)findViewById(R.id.loginButton);
         barcodeButton = (LinearLayout)findViewById(R.id.barcodeButton);
@@ -198,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(Review result) {
                 //등록 된 상품이 없는 경우
                 if (result == null) {
-                    if(MemberManager.getInstance().getIsLogin() == false){
+                    if (MemberManager.getInstance().getIsLogin() == false) {
                         Toast.makeText(MainActivity.this, "로그인을 먼저 하세요.", Toast.LENGTH_LONG).show();
                         Intent i = new Intent(MainActivity.this, LoginActivity.class);
                         i.putExtra("fromRegister", "fromRegister");
@@ -207,9 +225,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                     Intent i = new Intent(MainActivity.this, ProductRegisterActivity.class);
                     startActivity(i);
-                } else if(result.productInfo.productName.equals("nothingApply")){
+                } else if (result.productInfo.productName.equals("nothingApply")) {
                     Toast.makeText(MainActivity.this, "이미 등록 요청 된 상품입니다.", Toast.LENGTH_LONG).show();
-                }else { //리뷰 get
+                } else { //리뷰 get
                     Intent i = new Intent(MainActivity.this, ReviewActivityTest.class);
                     ScanManager.getInstance().setProductUrl(result.productInfo.productUrl);
                     i.putExtra("Result", result);
@@ -263,5 +281,68 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /**
+     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
+     */
+    //GCM get token
+    public void getInstanceIdToken() {
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    /**
+     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
+     */
+    public void registBroadcastReceiver(){
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
+                    // 액션이 READY일 경우
+
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
+                    // 액션이 GENERATING일 경우
+
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
+                    // 액션이 COMPLETE일 경우
+                    token = intent.getStringExtra("token");
+
+                }
+            }
+        };
+    }
+
+    /**
+     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
+     */
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 }
